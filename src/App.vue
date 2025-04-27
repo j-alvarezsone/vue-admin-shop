8e1d071fc9df4eea05b5b631239dec8efd5bc678
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-04-27 11:29:09 +0200

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <h1>
+    Hello World
+  </h1>
+  <RouterView />
+</template>
+
+<style scoped></style>
