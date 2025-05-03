9baa1cb636a56e6b029ba19e7f8869a53c3cbc09
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-05-04 01:05:01 +0200

    feat: add headers configuration to axios instance in tesloApi

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -3,6 +3,10 @@ import axios from "axios";
 
 const tesloApi = axios.create({
   baseURL: env.VITE_TESLO_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
 });
 
 tesloApi.interceptors.request.use((config) => {
